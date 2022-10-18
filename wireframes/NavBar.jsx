ea3a87af4b4eb4ef4d5c2cd613be439a70a4b226
kevinmsmith131 <kevinmsmith131@gmail.com>
2022-10-18 17:35:43 -0400

    removed node_modules and added them to gitignore

---
+++ wireframes/NavBar.jsx
@@ -0,0 +1,41 @@
+import react from 'react';
+import styled from 'styled-components';
+import { Chat } from '@styled-icons/material/Chat';
+import { School } from '@styled-icons/material/School';
+import { Home } from '@styled-icons/material/Home';
+import { Person } from '@styled-icons/material/Person';
+
+const NavBar = () => {
+  return (
+    <div>
+      <StyledChat />
+      <StyledSchool />
+      <StyledHome />
+      <StyledPerson />
+    </div>
+  );
+}
+
+const StyledChat = styled(Chat)`
+  color: white;
+`
+
+const StyledSchool = styled(School)`
+  color: white;
+`
+
+const StyledHome = styled(Home)`
+  color: white;
+`
+
+const StyledPerson = styled(Person)`
+  color: white;
+`
+
+export const StyledNavBar = styled(NavBar)`
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  width: 100%;
+  height: 100px;
+`
