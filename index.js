a0d81e419785a634f1ffd7fc0e9043784a4bb468
Raunak <raunakb22@gmail.com>
2022-12-11 22:27:15 -0500

    learning page

--- index.js
+++ index.js
@@ -42,6 +42,54 @@ app.get("/profile/:email", async function (req, res) {
     res.send("Error " + err);
   }
 });
+
+app.get("/learning", async (req, res) => {
+  try{
+  const client = await pool.connect();
+  const result = await client.query("SELECT * FROM postinfo ORDERBY modified;");
+  const results = {posts: result ? result.rows : null};
+  if(result != null){
+    
+    results.posts = results.posts.map(post => {
+      const obj = {header:post.header, init: "", currMod: "", replies: 0, author: "A Ferrara"};
+      const date = new Date();
+  
+      const currDay = date.getDate();
+      const currMonth = date.getMonth() + 1;
+      const currYear = date.getFullYear();
+
+      const created = post.created.split("-");
+      
+      if(currMonth > created[1])
+        obj.init = "a month ago";
+      else if(+created[2] - +currDay == 0)
+        obj.init = "Today";
+      else if(+created[2] - +currDay == 1)
+        obj.init = "1 day ago";
+      else
+        obj.init = +created[2] - +currDay + " days ago";
+
+      const modified = post.modified.split("-");
+
+      if(currMonth > modified[1])
+        obj.currMod = "a month ago";
+      else if(+modified[2] - +currDay == 0)
+        obj.currMod = "Today";
+      else if(+modified[2] - +currDay == 1)
+        obj.currMod = "1 day ago";
+      else
+        obj.currMod = +modified[2] - +currDay + " days ago";
+    });
+    res.render("pages/learning", posts);
+  }
+  client.release();
+  }
+  catch(err){
+    console.error(err);
+    res.send("Error " + err);
+  }
+});
+
 app
   .get("/db", async (req, res) => {
     try {
---
+++ index.sql
@@ -0,0 +1,11 @@
+INSERT INTO POSTINFO (header, body, category, created, modified)
+  VALUES('Digestion Organs','Which organ breaks apart and grinds up food that enters it from the mouth via the esophagus?', 'Biology', '2022-11-06', '2022-11-29');
+
+INSERT INTO POSTINFO (header, body,category, created, modified)
+  VALUES('Miscibility help solutions A,B,C', 'I have liquids A,B, and C. B is soluble in both A and C, but A and C are insoluble in each other. If I add B to A and C, what happens?', 'Chemistry', '2022-11-28', '2022-11-28');
+
+INSERT INTO POSTINFO (header, body,category, created, modified)
+  VALUES('How to find the slope?', 'Title', 'Mathematics', '2022-11-29', '2022-12-03');
+
+INSERT INTO POSTINFO (header, body,category, created, modified)
+  VALUES('What is a black hole exactly?', 'Title', 'Mathematics', '2022-11-29', '2022-12-03');
