de6ad3d8a52177f23a5e6bbd64774c65ea361b7a
Raunak <raunakb22@gmail.com>
2022-12-12 00:47:22 -0500

    Add post checks out

--- controllers/mainposts.js
+++ controllers/mainposts.js
@@ -7,13 +7,17 @@ const pool = new Pool({
   },
 });
 
+exports.addPostGet = async (req, res) => {
+  res.status(200).redirect("/learning");
+};
+
 exports.addPost = async (req, res) => {
   try{
     const {category, header} = req.body;
     const date = new Date();
 
     pool.query("INSERT INTO postinfo (header, category, created, modified) VALUES ($1, $2, $3, $3)",
-      [header, category, date, date],
+      [header, category, date],
       (error, result) => {
         res.status(200).redirect("/learning");
       });
--- routes/post.js
+++ routes/post.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const userController = require("../controllers/mainposts");
 const router = express.Router();
+router.get("/add", userController.addPostGet);
 router.post("/add", userController.addPost);
 module.exports = router;
